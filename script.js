2efe5944a841afceaa17b349ef93b291ed943ec6
Gabriele Salvo <72619479+gabsalvo@users.noreply.github.com>
2021-03-04 14:00:15 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,98 @@
+'use strict';
+/*
+1. Create variables to not call doc query each time --Done
+2. Create Random selection of dice dynamically
+3. Create current point based on dice numb rolled
+4.  Create general array scores --Done
+4.1 Create switch player
+5. Hold button to add current point to total point, make current point 0 and switch player
+6.New GAme --Done
+*/
+//VARIABLES CONST
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+let scores, currentScore, activePlayer, playing;
+
+//INITIAL CONDITION OF THE GAME
+const init = function () {
+  playing = true;
+  currentScore = 0;
+  activePlayer = 0;
+  scores = [0, 0];
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+  player0El.classList.add('player--active');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player.active');
+  player1El.classList.remove('player--winner');
+};
+init();
+
+//FUNCTIONS
+
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
+
+//CREATE RANDOM GENERATED NUMBER WHEN ROLLING DICE BETWEEN 1 AND 6
+btnRoll.addEventListener('click', function () {
+  if (playing) {
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    diceEl.src = `dice-${dice}.png`;
+    diceEl.classList.remove('hidden');
+
+    if (dice !== 1) {
+      currentScore += dice;
+      document.getElementById(
+        `current--${activePlayer}`
+      ).textContent = currentScore;
+    } else {
+      switchPlayer();
+    }
+  }
+});
+
+//HOLD BUTTON TO SUM UP POINT AND SWITCH PLAYER
+btnHold.addEventListener('click', function () {
+  if (playing) {
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+
+    if (scores[activePlayer] >= 100) {
+      playing = false;
+      diceEl.classList.add('hidden');
+
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+    } else {
+      switchPlayer();
+    }
+  }
+});
+
+//NEW GAME BOTTON
+btnNew.addEventListener('click', init);
